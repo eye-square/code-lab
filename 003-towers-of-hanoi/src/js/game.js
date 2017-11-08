15c4f8a329c07ea4abfc70548f685f0464d0164d
JohannesMerz <johannes_merz@gmx.de>
2017-11-08 16:35:42 +0100

    implements toh game engine except of the move command

--- 003-towers-of-hanoi/src/js/game.js
+++ 003-towers-of-hanoi/src/js/game.js
@@ -1,18 +1,52 @@
 
-function createGame(setup) {
-  // init game, create the rods and disks
-  const state = {
-    // todo add the intial game state
-  };
+window.createGame = function createGame(setup) {
+  function createGameState() {
+    const newState = {
+      rods: [
+        [],
+        [],
+        [],
+      ],
+      numberofDisks: setup.disks,
+      solved: false,
+    };
+
+    for (let i = 0; i < setup.disks; i += 1) {
+      newState.rods[0][i] = { disk: i };
+    }
+
+    return newState;
+  }
+
+  let state = createGameState();
+  const listeners = [];
+
+  function notifyListeners() {
+    for (let i = 0; i < listeners.length; i += 1) {
+      listeners[i](state);
+    }
+  }
 
   return {
     // moves one disk from one rod to antoher,
     // returns true for valid move, false for invalid move
     move(command) {
-      // alter the game state if the command is valid
+      console.log(`moving disk from ${command.from} to ${command.to}`);
+      // TODO implement move
+      // 1. check if the command is valid
+      // 2. remove the disk from the origin rod
+      //      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/pop
+      // 3. add the disk to the top(end) of the new rod
+      //      https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
+      // 4. check if the game is solved and update the state if so
+      // 5. notifyListeners
+      // 6. return true/false depending if the move was valid or not
     },
     // resets the game to the start again
-    reset() {},
+    reset() {
+      state = createGameState();
+      notifyListeners();
+    },
     // returns the game state
     getState() {
       return state;
@@ -20,7 +54,7 @@ function createGame(setup) {
     // registers a listener that gets called everytime the
     // game changes
     addListener(listener) {
-
+      listeners.push(listener);
     },
   };
-}
+};
--- 003-towers-of-hanoi/src/js/index.js
+++ 003-towers-of-hanoi/src/js/index.js
@@ -1,1 +1,9 @@
 console.log('this will be towers of hanoi - the game');
+
+const myGame = window.createGame({ disks: 3 }); // create a new game with 3 disks (default)
+
+console.log('initial state', myGame.getState());
+
+myGame.addListener((gameState) => {
+  console.log('gameState changed', gameState);
+});
